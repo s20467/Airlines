397f7149a5c325ae1e0f2cb7bdc7b5bea23c2d19
Hubert Orzełowski <s20467@pjwstk.edu.pl>
2022-05-23 14:33:44 +0200

    Create repository for PartnerCompany entity class

---
+++ src/main/java/com/example/airlines/repository/PartnerCompanyRepository.java
@@ -0,0 +1,7 @@
+package com.example.airlines.repository;
+
+import com.example.airlines.model.PartnerCompany;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartnerCompanyRepository extends JpaRepository<PartnerCompany, Integer> {
+}
